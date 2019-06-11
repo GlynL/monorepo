f5bb7fb0b72bec81177e0ab99961aae9edb7af72
Glyn L <glynlewington@gmail.com>
2019-06-11 13:31:30 +1000

    bug repo

---
+++ packages/mobile/App.js
@@ -0,0 +1,20 @@
+import common from "common";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app! {common}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
